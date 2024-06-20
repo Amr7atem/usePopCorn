da074161df8d58c2bbc5a0a37c29b4334c558543
Amr7atem <amrhatemanwar@gmail.com>
2024-06-20 21:34:56 +0300

    http edited to https

--- src/App.js
+++ src/App.js
@@ -237,7 +237,7 @@ function MovieDetails({ selctedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selctedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selctedId}`
         );
         const data = await res.json();
         setMovie(data);
