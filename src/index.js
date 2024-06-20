5cbb6428a5b438ea128ea2262565997e37a7fab0
Amr7atem <amrhatemanwar@gmail.com>
2024-06-20 21:20:19 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+// import StarRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      color="red"
+      defaultRating={3}
+    /> */}
+  </React.StrictMode>
+);
---
+++ src/useMovies.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+
+const KEY = "334c7d3e";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      // callback?.();
+
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong when fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw Error("Movie not found");
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      // This is a Clean up function
+      return function () {
+        controller.abort();
+      };
+    },
+
+    [query]
+  );
+  return { movies, isLoading, error };
+}
